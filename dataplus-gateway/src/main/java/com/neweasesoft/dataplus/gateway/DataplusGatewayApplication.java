d3f79411d9b7a0e2e5cd296a29c1929772ad90ac
fushuwei <331044761@qq.com>
2022-09-12 17:13:55 +0800

    创建项目

---
+++ dataplus-gateway/src/main/java/com/neweasesoft/dataplus/gateway/DataplusGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.neweasesoft.dataplus.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataplusGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataplusGatewayApplication.class, args);
+	}
+
+}
---
+++ dataplus-gateway/src/test/java/com/neweasesoft/dataplus/gateway/DataplusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neweasesoft.dataplus.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataplusGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
